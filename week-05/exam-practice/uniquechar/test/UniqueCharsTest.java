9315dafe3d4057e589da64d6d287542ea3184e3a
Balazs Salfay <balazs.salfay@gmail.com>
2017-12-21 17:32:47 +0100

    uniquechar test added

--- week-05/exam-practice/uniquechar/test/UniqueCharsTest.java
+++ week-05/exam-practice/uniquechar/test/UniqueCharsTest.java
@@ -1,14 +1,30 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class UniqueCharsTest {
-  @Test
-  public void main() throws Exception {
-  }
+
+  UniqueChars test = new UniqueChars();
 
   @Test
   public void uniqueCharacters() throws Exception {
+    String word = "anagram";
+    assertEquals(test.uniqueCharacters(word), new ArrayList(Arrays.asList('n', 'g', 'r','m')));
   }
 
+  @Test
+  public void uniqueCharacters2() throws Exception {
+    String word = "barack";
+    assertEquals(test.uniqueCharacters(word), new ArrayList(Arrays.asList('b', 'r', 'c','k')));
+  }
+
+  @Test
+  public void uniqueCharacters3() throws Exception {
+    String word = "elephant";
+    assertEquals(test.uniqueCharacters(word), new ArrayList(Arrays.asList('l', 'p', 'h','a', 'n', 't')));
+  }
 }
