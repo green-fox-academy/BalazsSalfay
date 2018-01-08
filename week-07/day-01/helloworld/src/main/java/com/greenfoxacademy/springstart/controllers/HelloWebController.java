ab9902d976cbad7682696e69c985ff50c9c330bd
Balazs Salfay <balazs.salfay@gmail.com>
2018-01-08 17:17:39 +0100

    feat(helloworld) colorized text feater added

--- week-07/day-01/helloworld/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-07/day-01/helloworld/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -19,14 +19,17 @@ public class HelloWebController {
           "Dia dhuit", "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry",
           "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej", "Sa-wat-dee", "Merhaba", "Selam",
           "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+  String[] colors = {"red", "orange", "blue", "green", "purple", "grey", "brown", "gold", "khaki", "magenta", "navy"};
 
   @RequestMapping(value="/web/greeting")
   public String greeting(Model model, @RequestParam(value="name")String name) {
     counter2.incrementAndGet();
     int randomNumber = (int)(Math.random() * hellos.length);
+    int randomNumber2 = (int) (Math.random() * colors.length);
     model.addAttribute("hello",hellos[randomNumber]);
     model.addAttribute("name", name);
     model.addAttribute("counter2", counter2);
+    model.addAttribute("color", colors[randomNumber2]);
     return "greeting";
   }
 }
