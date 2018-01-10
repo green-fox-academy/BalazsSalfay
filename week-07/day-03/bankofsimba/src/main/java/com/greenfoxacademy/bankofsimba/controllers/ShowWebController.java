5b7a831c264bb2de944d0d5dbe4b8d1ded7223a7
Balazs Salfay <balazs.salfay@gmail.com>
2018-01-10 14:54:48 +0100

    feat(bankofsimba) utext exercise added

--- week-07/day-03/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controllers/ShowWebController.java
+++ week-07/day-03/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controllers/ShowWebController.java
@@ -5,11 +5,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class ShowWebController {
   BankAccount firstAccount = new BankAccount("Simba", 2000.00, "lion");
+  List<BankAccount> accounts = new ArrayList<>();
 
-  @RequestMapping (value="/show")
+  @RequestMapping (value="/oneaccount")
   public String showingAnAccount (Model model) {
     model.addAttribute("name", firstAccount.getName());
     model.addAttribute("balance", firstAccount.getBalance());
@@ -19,7 +23,15 @@ public class ShowWebController {
   }
 
   @RequestMapping(value="/exception")
-  public String showException () {
-    return "exception";
+  public String showException (Model model) {
+    model.addAttribute("format1", "<em>HTML</em>");
+    model.addAttribute("format2","<b>Enjoy yourself!</b>");
+    return "except";
+  }
+
+  @RequestMapping(value="/multiaccounts")
+  public String showMultiAccount (Model model) {
+
+    return "listaccount";
   }
 }
